144f96ef61a998586fda1750383c82d05782895c
rmm <rob.mills@epfl.ch>
2025-04-17 15:39:11 +0200

    chore: simplified, tidied and added short docstring

--- sync_dataset.py
+++ sync_dataset.py
@@ -1,11 +1,20 @@
+'''
+This script updates any new observations into the combined dataset 
+stored on Hugging Face Hub.
+
+If the operation fails, it exits with non-zero status so the workflow
+fails too (better observability of the failures)
+'''
+
 import os 
 from huggingface_hub import HfApi
 from src.dataset_handling import sync_dataset
 
-
+############################################################
 dataset_id = "Saving-Willy/temp_dataset"
 token = os.getenv("HF_TOKEN")
 dataset_filename = "data/train-00000-of-00001.parquet"
+############################################################
 
 if __name__ == '__main__':
     # Initialize API client
@@ -16,19 +25,7 @@ if __name__ == '__main__':
         api, dataset_id, dataset_filename, 
         create_dataset_if_not_exists=False)
 
-    if n_new is not None:
-        if n_new == 0:
-            print(f"Info: no action taken, no new observation files found")
-        else:
-            # print the number of new files added
-            print(f"Added {n_new} new files to dataset {dataset_id}.")
-    else:
+    if n_new is None:
         # something went wrong. - want the workflow to fail so nonzero exit  
         print(f"Failed to sync dataset {dataset_id}.")
         raise SystemExit(1) 
-    
-    
-    
-    
-
-    
