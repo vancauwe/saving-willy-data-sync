ba9df439c1ab1166b77bf10275915c6361bf2f44
rmm <rob.mills@epfl.ch>
2025-04-17 15:45:22 +0200

    chore: cleanup and document the example script

--- src/example_manual_cleanup.py
+++ src/example_manual_cleanup.py
@@ -1,21 +1,26 @@
-import json
+'''
+This script shows how to manually clean up the dataset, through the 
+example of filtering out observations from a specific user, and 
+rebuilding the dataset from scratch. 
+
+'''
 import os 
 import pandas as pd
-
 from huggingface_hub import HfApi
-
 from dataset_handling import create_blank_dataset, lookup_json_files, add_json_files_to_metadata
 
+############################################################
+dataset_id = "Saving-Willy/temp_dataset"
+token = os.getenv("HF_TOKEN")
+dataset_filename = "data/train-00000-of-00001.parquet"
+############################################################
 
 if __name__ == '__main__':
     # example usage, with no actions taken by the demo
     # - see also `reset_dataset_rebuild_from_json`, which wraps most of these 
     #   steps, but doesn't implement the filtering.
-    token = os.getenv("HF_TOKEN")
-    api = HfApi(token=token)
 
-    data_files = "data/train-00000-of-00001.parquet"
-    dataset_id = "Saving-Willy/temp_dataset"
+    api = HfApi(token=token)
     
     # find the uploaded observation+classification files
     all_json_files = lookup_json_files(api, dataset_id)
